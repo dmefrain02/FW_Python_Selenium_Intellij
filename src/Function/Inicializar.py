ee3e4695c8155c0788663225a7c1e9cc6e7de944
dmefrain02 <dmefrain.02@gmail.com>
2024-07-28 11:15:59 -0600

    FW_Pruebas_Python_Selenium
    Montado en Intellij

---
+++ src/Function/Inicializar.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import os
+
+class Inicializar():
+
+    URL_SeleniumGrid = "http://localhost:4444/wd/hub"
+    Navegadores_Sel_Grid = ["Chrome","Edge_Remote","Chrome_Remote"]
+    
+    #Directorio Base - Obtiene la ruta del directorio src del proyecto
+    BaseDir = os.path.abspath(os.path.join(__file__,"../.."))
+
+    #Tiempo de espera utilizado dentro del Framework
+    Tiempo_Espera = 1
+    
+    #Pagina en nuevo tab abierto
+    Page_Tab = 'about:blank'
+    
+    #Rutas utilizadas dentro del Framework
+    Archivo_Cargar = BaseDir + u'\Archivos a Cargar\software-quality.png'
+    Ruta_Descarga = BaseDir + u'\Archivos Descargados'
+    Archivo_Descargado = "requirements.txt"
+    Bitacora= BaseDir + u'\Archivos Descargados\Pruebas Descargas Archivos.txt'
+    Imagenes_Cortadas = BaseDir + u'\Data\Imagenes Cortadas'
+    Ruta_Grabacion= BaseDir + u'\Data\Grabaciones'
+    Path_Evidencias = BaseDir + u'\Data\Capturas'
+    TestCase_x_Context = "S"
+    
+    #Directorio Json
+    Json = BaseDir + u'\Pages'
+    JsonRespondata = BaseDir + u'\Data\Json'
+    
+    #Formato Hora y Fecha
+    DateFormat = '%d-%m-%Y'
+    HourFormat = '%H%M%S'
+    
+    #Navegador a Utilizar
+    Navegador = 'Chrome'
+    
+    #Ruta Excel para escribir resultados o leer datos
+    Excel_Leer_Escribir = BaseDir + u'\Data\Pruebas1.xlsx'
+    
+    #Ruta Excel para crear excel
+    Excel_Crear = BaseDir + u'\Data'
+    
+    #Ambientes de Pruebas
+    #Enviroment == 'Dev':
+    URL_Dev = f'' #La f solo va si requerimos pasarle parametros en el link
+    USER_Dev = ''
+    DB_HOST_Dev = 'EFRAIN-CD\EFRAINCD'
+    DB_PORT_Dev = '1433'
+    DB_DATABASE_Dev = 'Pruebas_Automatizadas_Python_DEV'
+    DB_USER_Dev = 'EFRAIN_ACD'
+    DB_PASS_Dev = '114660137'
+    
+    #Enviroment == 'QA':    
+    URL_QA = f'' #La f solo va si requerimos pasarle parametros en el link
+    USER_QA = ''
+    DB_HOST_QA = 'EFRAIN-CD\EFRAINCD'
+    DB_PORT_QA = '1433'
+    DB_DATABASE_QA = 'Pruebas_Automatizadas_Python_QA'
+    DB_USER_QA = 'EFRAIN_ACD'
+    DB_PASS_QA = '114660137' 
+    #Enviroment == 'UAT':
+    URL_UAT = f'' #La f solo va si requerimos pasarle parametros en el link
+    USER_UAT = ''
+    DB_HOST_UAT = 'EFRAIN-CD\EFRAINCD'
+    DB_PORT_UAT = '1433'
+    DB_DATABASE_UAT = 'Pruebas_Automatizadas_Python_UAT'
+    DB_USER_UAT = ''
+    DB_PASS_UAT = '' 
+    #Enviroment == 'PROD':
+    URL_PROD = f'' #La f solo va si requerimos pasarle parametros en el link
+    USER_PROD = ''
+    DB_HOST_PROD = 'EFRAIN-CD\EFRAINCD'
+    DB_PORT_PROD = '1433'
+    DB_DATABASE_PROD = 'Pruebas_Automatizadas_Python_PROD'
+    DB_USER_PROD = ''
+    DB_PASS_PROD = '' 
